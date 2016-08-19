76089b04ad8701591718f4e2aa481d82b0b9b173
Yandi Kurniawan <yandikurniawan@gmail.com>
2016-08-20 00:25:51 +0700

    Preparing database and base layout

---
+++ database/migrations/2016_08_18_074526_create_villages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVillagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('villages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('districts_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('villages');
+    }
+}
